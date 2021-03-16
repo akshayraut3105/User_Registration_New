14010c41cfbe5ad01c33e8b0c62dae083ce37a6c
akshayraut3105 <akshayraut3105@gmail.com>
2021-03-16 17:34:43 +0530

    [AKSHAY] added password type 2

--- registration_regex/RegexValidation.cs
+++ registration_regex/RegexValidation.cs
@@ -12,7 +12,8 @@ namespace registration_regex
         public const string Regex_Email = "^[0-9A-Za-z]+([._+-][0-9A-Za-z]+)*[@][0-9A-Za-z]+.[a-zA-Z]{2,3}(.[a-zA-Z]{2})?$";
         public const string Regex_MobileNumber = "^[1-9]{1}[0-9]{0,3}\\s[1-9]{1}[0-9]{9}$";
         //  (\S)is use for  matches any non-whitespace character
-        public const string Regex_Password = "[\\S]{8,}";
+        // public const string Regex_Password = "[\\S]{8,}";
+        public const string Regex_Password = "^(?=.{8,}$)(?=.*[A - Z]).*$";
         public bool ValidateFirstName(string FirstName)
         {
             return Regex.IsMatch(FirstName, Regex_FirstName);
