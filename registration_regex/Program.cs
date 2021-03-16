347088592de3d3d8dbca6076dab0dd220b9a6553
akshayraut3105 <akshayraut3105@gmail.com>
2021-03-16 17:20:41 +0530

    [AKSHAY] added email

--- registration_regex/Program.cs
+++ registration_regex/Program.cs
@@ -8,12 +8,15 @@ namespace registration_regex
         {
             Console.WriteLine("Welcome to my registration form regex apllication by Akshay");
             RegexValidation regexValidation = new RegexValidation();
-            Console.Write("Enter Your First Name::-");
+            Console.WriteLine("Enter Your First Name::-");
             string FirstName = Console.ReadLine();
             Console.WriteLine(regexValidation.ValidateFirstName(FirstName));
-            Console.Write("Enter Your Last Name::-");
+            Console.WriteLine("Enter Your Last Name::-");
             string LastName = Console.ReadLine();
             Console.WriteLine(regexValidation.ValidateLastName(LastName));
+            Console.WriteLine("Enter Your Email Address::-");
+            string Email = Console.ReadLine();
+            Console.WriteLine(regexValidation.ValidateEmail(Email));
         }
     }
 }
