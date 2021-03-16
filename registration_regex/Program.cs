3b5d43e5ad8f1857093ad765387ee4ed5d2c8574
akshayraut3105 <akshayraut3105@gmail.com>
2021-03-16 16:35:05 +0530

    [AKSHAY] added last name

---
+++ registration_regex/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace registration_regex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("====Welcome to User Registration Problem");
+            RegexValidation regexValidation = new RegexValidation();
+            Console.Write("Enter Your First Name::-");
+            string FirstName = Console.ReadLine();
+            Console.WriteLine(regexValidation.ValidateFirstName(FirstName));
+            Console.Write("Enter Your Last Name::-");
+            string LastName = Console.ReadLine();
+            Console.WriteLine(regexValidation.ValidateLastName(LastName));
+        }
+    }
+}
